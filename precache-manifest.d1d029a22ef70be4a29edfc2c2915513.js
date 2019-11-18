6bd53738660f3adb8c81395efe42eaa9d0c4a1b0
pauluzo <45356331+pauluzo@users.noreply.github.com>
2019-11-18 04:24:33 +0100

    Updates

---
+++ precache-manifest.d1d029a22ef70be4a29edfc2c2915513.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821711568f30fdf10fe510a1f5dc69b8",
+    "url": "/demo-app/index.html"
+  },
+  {
+    "revision": "1ee95919aa149a4e92d8",
+    "url": "/demo-app/static/css/main.751b24fa.chunk.css"
+  },
+  {
+    "revision": "b820e461fa38f0a1f1de",
+    "url": "/demo-app/static/js/2.906c217a.chunk.js"
+  },
+  {
+    "revision": "1ee95919aa149a4e92d8",
+    "url": "/demo-app/static/js/main.4fbbb13c.chunk.js"
+  },
+  {
+    "revision": "709cf7be6b5deeb3eb7b",
+    "url": "/demo-app/static/js/runtime-main.6dbf2af7.js"
+  }
+]);
